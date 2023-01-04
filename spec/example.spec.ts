35893855193503e53031627c0de399e1a47b7a9d
GitSiva1991 <sivatesting91@gmail.com>
2023-01-04 15:49:45 +0530

    TestSerenityWdio

--- spec/example.spec.ts
+++ spec/example.spec.ts
@@ -5,13 +5,10 @@ import { describe, it } from 'mocha';
 
 describe('serenity-js.org website', () => {
 
-    const acceptCookiesButton = PageElement.located(By.css('.cookieinfo-close')).describedAs('accept cookies button')
-
     it(`tells people what they're reading`, () =>
         actorCalled('Alice').attemptsTo(
             Navigate.to('https://google.com'),
-            Click.on(acceptCookiesButton),
             
-            Ensure.that(Page.current().title(), includes('Serenity/JS')),
+            Ensure.that(Page.current().title(), includes('Google')),
         ));
 });
