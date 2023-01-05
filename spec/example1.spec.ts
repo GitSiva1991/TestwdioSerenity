a892938087e569311808fe29583420487fcbd143
GitSiva1991 <sivatesting91@gmail.com>
2023-01-05 13:17:23 +0530

    TestBranchSerenity

---
+++ spec/example1.spec.ts
@@ -0,0 +1,14 @@
+import { Ensure, includes } from '@serenity-js/assertions';
+import { actorCalled } from '@serenity-js/core';
+import { By, Click, Navigate, Page, PageElement } from '@serenity-js/web';
+import { describe, it } from 'mocha';
+
+describe('serenity-js.org website', () => {
+
+    it(`tells people what they're reading`, () =>
+        actorCalled('Alice').attemptsTo(
+            Navigate.to('https://jmeter.com'),
+            
+            Ensure.that(Page.current().title(), includes('Google')),
+        ));
+});
